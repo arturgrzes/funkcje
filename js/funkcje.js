7ec2a9408779f6e02d52c2233643e2e10b874e79
Artur Grzes <grzesa@poczta.onet.pl>
2017-06-10 14:57:33 +0200

    2

--- js/funkcje.js
+++ js/funkcje.js
@@ -1,13 +1,9 @@
 'use strict'
 
 
-function multiply(parametr1, parametr2, parametr3) {
-    var result;
-    result = parametr1 * parametr2 * parametr3;
-    return result;
-}
-var wynik = multiply(35, 2, 10);
-console.log("Wynik: " + wynik);
+var funkcja = function () {
+    return"wynik:)";
+};
 
-var wynik = multiply(10, 2, 10);
-console.log("Wynik: " + wynik);
+console.log( funkcja );
+console.log() );
